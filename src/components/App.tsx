e2c31ab8ef021b0bcf65d95e8800e8cf71dbb04a
moris88 <morist88@outlook.it>
2023-08-24 19:04:41 +0200

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  const test = (): void => {
+    debugger
+  }
+  test()
+  return <></>
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        // for example, lint .ts and .tsx
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
+    }),
+  ],
+})
